93ab53e5cbf9195a3b2e3ed5d97a5fcb994e3938
Kumaran <kthanaraj@altimetrik.com>
2024-01-23 16:45:09 +0530

    Updated few styles

--- src/components/Models/Models.styled.tsx
+++ src/components/Models/Models.styled.tsx
@@ -1,18 +1,21 @@
 import { styled } from "styled-components";
 
 export const CardContent = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    background: lightgrey;
+  display: flex;
+  flex-wrap: wrap;
+  background: lightgrey;
 `;
 
 export const Card = styled.div`
-    margin: 10px;
-    flex-basis: 18%;
-    cursor: pointer;
+  margin: 10px;
+  flex-basis: 16%;
+  cursor: pointer;
+  background: white;
+  border-radius: 10px;
+  padding: 10px;
 `;
 
 export const CardImg = styled.img`
-    height: 100px;
-    width: 150px;
+  height: 100px;
+  width: 150px;
 `;
